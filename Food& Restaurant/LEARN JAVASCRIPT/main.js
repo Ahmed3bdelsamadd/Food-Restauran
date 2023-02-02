0e5bb6ffba1588db2b6e4e39e2f1f38510dad4b9
Ahmed Abd El Samad <121310690+Ahmed3bdelsamadd@users.noreply.github.com>
2023-02-03 01:05:44 +0200

    Add files via upload

---
+++ Food& Restaurant/LEARN JAVASCRIPT/main.js
@@ -0,0 +1,287 @@
+window.onload= function () {
+    document.querySelector("h1").style.color="green";
+};
+//.window.alert("hello from js file");
+document.write(" <h1> Hello <span> page </span> </h1>");
+
+console.logo ( "Hello from js file");
+/*---------directive----------%c
+*/
+console.logo("Hello from %c  Js File", "color:red; front size :40 px" );
+
+/*-----------   Es6 ----------------*/
+/*----------- data type indro 
+-------------string ---------------
+-----------numder --------------
+- --------------arrary =>object --------------
+----------------------object----
+---------------------boolean--------------
+viraboles intro-----
+--*/
+console.loge('Ahmed'); 
+console.loge(typeof{name:'ahmed' , age:17 ,country:'Eg'}); 
+console.loge(typeof true);
+console.loge(typeof false);
+console.loge(typeof undefined);
+console.loge(typeof null);
+
+var user="ahmed";
+var age = 19;
+
+
+console.loge('Ahmed'); 
+console.loge('Ahmed'); 
+console.loge('Ahmed'); 
+console.loge('Ahmed'); 
+console.loge('Ahmed'); 
+console.loge('Ahmed'); 
+hello.innerhtml ="option";
+
+/* 0----------identifiers-------*/
+
+var userName="ahmed";
+
+
+/*
+-- var
+--- Radeclare-(yes)--
+---Access Before Declare-(undefined)
+---variable scope drama [Added To Window]-()
+---Block or function scope 
+
+
+-- Let 
+--- Radeclare-(no)--
+---Access Before Declare-()
+---variable scope drama [Added To Window]-()
+---Block or function scope 
+
+
+--const
+--- Radeclare-(no => error)--
+---Access Before Declare-()
+---variable scope drama [Added To Window]-()
+---Block or function scope
+ */
+
+var a = 1;
+var a =2;
+
+console.log(a);
+
+
+/*
+--string syntax .+. character Escape sequences
+--Escape .+. Line countinue 
+--/n 
+*/
+console.loge("Ahmed"/"Kattab");
+
+/*
+--concatenation
+*/
+let a ="We love ";
+
+
+document.write(a+" "+b);
+console.loge(a,b);
+
+
+/*
+--template lterals --
+*/
+
+let a ="We love ";
+let b="Java Script";
+let c ="and ";
+let d ="programming ";
+
+console.loge(a +""+ b +
+"\n"+ c +" "+ d);
+
+console.loge(`${a}""``\\ ${100*50}
+${c} ${d}`);
+
+let title = "AHMWD";
+let desc ="web school";
+let makeUp =
+<div class ="card">
+<div class = "child">
+<h2>${ title}</h2>
+<p> ${pragraph}</p>
+</div>
+</div>
+;
+document.write( makeUp);
+
+
+/*
+Arithmetic operators 
+--+-subtraction 
+-- *multiplication 
+--/Divison 
+--**Exponenttiation (Es7)
+--%Modulus (division.Remainder)
+--++Icrement .[post./.pre]
+...--Decrement.[post./.pre]
+*/ 
+
+console.logo(10+20);
+console.logo(10+"Ahmed");
+
+console.logo(typeof NaN);
+
+
+console.logo(10* 20);
+console.logo(10* -20);
+
+/*
+---+UNARY plus 
+..--+UNARY Negative
+*/
+
+console.loge(+100);
+console.loge(+"100");
+console.loge(+"-100");
+console.loge(+15.5);
+//console.loge(+0*ff); 
+//console.loge(+null);
+//console.loge(+false);
+//console.loge(+true);
+
+
+console.loge(-100);
+console.loge(+100);
+console.loge(+"100");
+console.loge(+"-100");
+console.loge(+15.5);
+//console.loge(-0*ff); 
+//console.loge(-null);
+//console.loge(-false);
+//console.loge(-true);
+
+
+console.loge(Number("100"));
+
+
+
+/*
+---Type corcion (type casting)
+---+
+----2
+---""
+---false ---true
+*/
+
+let a ="10";
+let l =20;
+let x =true;
+
+console.loge( a + b);
+
+console.log(false-true);
+
+
+
+/*---Number----*/
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 *10 *10* 10* 10* 10);
+
+
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+
+
+
+/*---Number methods----*/
+
+console.log(Number.parseInt(100,Ahmed));
+console.log(parseFloat("100.500 Ahmed "));
+console.log(Number.isInteger(100.500));
+console.log(Number.isNaN("Ahmed"));
+
+/*----string methods ----*/
+
+/*----Logical operator ----*/
+
+console.loge(!(10 =="10"));
+
+console.loge(!(10 =="10") && 10>8 && 10>50);
+
+console.loge(!(10 =="10") || 10>8 ||10>50);
+
+
+/*----control flow  ----
+
+if (conditian) {
+\\ block  of code
+}
+*/
+let price =100;
+let discount =false;
+let discountAmount=30;
+let country = Egypt;
+let students =true;
+
+
+if(discount=== true){ price-=-30}
+else if (country= egypt ){price = -30};
+if (student==true){
+    price -=discountAmoun=+30
+};
+
+/*----Nasted if----*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
